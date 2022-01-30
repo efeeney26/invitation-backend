1a586967a8363f28dd9a589f6e9c2a0c8a9b3a18
Kochkin Danila <>
2022-01-30 13:53:49 +0300

    init

---
+++ index.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
