8293ab4cce13cda75e4fc02d61aa8c6179848849
Kochkin Danila <>
2022-02-21 14:51:15 +0300

    добавил cors

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { config } from 'dotenv';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
+import cors from 'cors'
 
 import dbConfig from './db/config';
 import guestsRouter from './routes/guests';
@@ -12,6 +13,10 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: ['https://efeeney26.github.io/invitation-admin-frontend'],
+    optionsSuccessStatus: 200
+}))
 
 app.use('/api/guests', guestsRouter);
 
